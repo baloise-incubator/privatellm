104e892feeb94f82ff492e2716a26136c2e27838
Patrick Ritter <7950125+patrit@users.noreply.github.com>
2023-10-16 15:58:29 +0200

    initial chat function with openapi

--- privatellm/main.py
+++ privatellm/main.py
@@ -2,6 +2,13 @@ from fastapi import FastAPI, File, UploadFile, Depends, HTTPException
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from typing import List
 import uvicorn
+from langchain.agents.agent_toolkits.openapi import planner
+import os
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from langchain.callbacks.manager import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 
 app = FastAPI()
 
@@ -12,6 +19,17 @@ security = HTTPBasic()
 fake_users_db = {
     "rr": "rr"
 }
+
+def assert_api_key():
+    api_key = os.environ.get("OPENAI_API_KEY", None)
+    if api_key is not None:
+        return
+    if os.path.exists("apikey.txt"):
+        api_key = open("apikey.txt", "r").read().strip()
+    if not api_key:
+        api_key = os.getenv("OPENAI_API_KEY")
+    if api_key:
+        os.environ["OPENAI_API_KEY"] = api_key
 
 # Verify user credentials
 def verify_credentials(credentials: HTTPBasicCredentials = Depends(security)):
@@ -40,6 +58,35 @@ async def upload_pdfs(
         filenames.append(pdf_file.filename)
 
     return {"uploaded_filenames": filenames}
+
+@app.post("/chat/")
+async def chat_trial(
+    input: str,
+    username: str = Depends(authenticate_user)
+):
+    api_key = assert_api_key()
+    template = """Question: {question}
+
+    Answer: Let's work this out in a step by step way to be sure we have the right answer."""
+
+    prompt = PromptTemplate(template=template, input_variables=["question"])
+
+    # Callbacks support token-wise streaming
+    callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
+
+    llm = ChatOpenAI(openai_api_key=api_key,
+        model_name="gpt-3.5-turbo", 
+        temperature=0.75,
+        max_tokens=2000,
+        top_p=1,
+        callback_manager=callback_manager, 
+        verbose=True, # Verbose is required to pass to the callback manager
+    )
+
+    llm_chain = LLMChain(prompt=prompt, llm=llm)
+    question = "What NFL team won the Super Bowl in the year Justin Bieber was born?"
+    
+    llm_chain.run(question)
 
 
 if __name__ == "__main__":
