3bbf5dc111198f63cc116ba4c12f870af4036d90
Leon König <22750370+koenigle@users.noreply.github.com>
2023-10-17 14:52:50 +0200

    add balck / mypy

--- privatellm/main.py
+++ privatellm/main.py
@@ -23,16 +23,17 @@ os.environ["ANONYMIZED_TELEMETRY"] = "False"
 
 # Configure the logging module
 logging.basicConfig(
-    level=logging.DEBUG,   # Logging level (DEBUG, INFO, WARNING, ERROR, CRITICAL)
-    format='%(asctime)s [%(levelname)s] %(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S'
+    level=logging.DEBUG,  # Logging level (DEBUG, INFO, WARNING, ERROR, CRITICAL)
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
 )
 
 
 class ModelEnum(Enum):
-    LLAMA = 'llama'
-    GPT4ALL = 'gpt4all'
-    CHATGPT = 'chatgpt'
+    LLAMA = "llama"
+    GPT4ALL = "gpt4all"
+    CHATGPT = "chatgpt"
+
 
 app = FastAPI()
 
@@ -40,9 +41,8 @@ app = FastAPI()
 security = HTTPBasic()
 
 # Users (for demonstration purposes, replace with your own authentication logic)
-fake_users_db = {
-    "rr": "rr"
-}
+fake_users_db = {"rr": "rr"}
+
 
 def assert_api_key():
     api_key = os.environ.get("OPENAI_API_KEY", None)
@@ -55,6 +55,7 @@ def assert_api_key():
     if api_key:
         os.environ["OPENAI_API_KEY"] = api_key
 
+
 # Verify user credentials
 def verify_credentials(credentials: HTTPBasicCredentials = Depends(security)):
     user = fake_users_db.get(credentials.username)
@@ -66,14 +67,15 @@ def verify_credentials(credentials: HTTPBasicCredentials = Depends(security)):
         )
     return credentials.username
 
+
 # Custom authentication dependency decorator
 def authenticate_user(username: str = Depends(verify_credentials)):
     return username
 
+
 @app.post("/uploadpdf/")
 async def upload_pdfs(
-    pdf_files: List[UploadFile],
-    username: str = Depends(authenticate_user)
+    pdf_files: List[UploadFile], username: str = Depends(authenticate_user)
 ):
     filenames = []
     for pdf_file in pdf_files:
@@ -83,12 +85,12 @@ async def upload_pdfs(
 
     return {"uploaded_filenames": filenames}
 
-@app.post("/chat/")
 
+@app.post("/chat/")
 async def chat(
     input: str,
     model_enum: ModelEnum = ModelEnum.LLAMA,
-    username: str = Depends(authenticate_user)
+    username: str = Depends(authenticate_user),
 ):
     template = """Question: {question}
 
@@ -107,7 +109,7 @@ async def chat(
                 max_tokens=2000,
                 top_p=1,
                 callback_manager=callback_manager,
-                verbose=True, # Verbose is required to pass to the callback manager
+                verbose=True,  # Verbose is required to pass to the callback manager
             )
         case ModelEnum.GPT4ALL:
             template = """"Question: {question}
@@ -115,36 +117,44 @@ async def chat(
                 Answer: Let's think step by step."""
 
             prompt = PromptTemplate(template=template, input_variables=["question"])
-            llm = GPT4All(model="gpt4all/model.bin", backend="gptj", callback_manager=callback_manager,
-                          verbose=True)
+            llm = GPT4All(
+                model="gpt4all/model.bin",
+                backend="gptj",
+                callback_manager=callback_manager,
+                verbose=True,
+            )
         case ModelEnum.CHATGPT:
             api_key = assert_api_key()
 
-            llm = ChatOpenAI(openai_api_key=api_key,
+            llm = ChatOpenAI(
+                openai_api_key=api_key,
                 model_name="gpt-3.5-turbo",
                 temperature=0.75,
                 max_tokens=2000,
                 top_p=1,
                 callback_manager=callback_manager,
-                verbose=True, # Verbose is required to pass to the callback manager
+                verbose=True,  # Verbose is required to pass to the callback manager
             )
         case _:
-            raise ValueError(f'unsupported model {model_enum}')
+            raise ValueError(f"unsupported model {model_enum}")
     llm_chain = LLMChain(prompt=prompt, llm=llm)
 
     resp = await llm_chain.arun(input)
     return resp.strip()
 
+
 async def populate_db(db):
     collection = db.get()
-    existing_docs = set([metadata['source'] for metadata in collection['metadatas']])
+    existing_docs = set([metadata["source"] for metadata in collection["metadatas"]])
     logging.info(f"existing documents {existing_docs}")
     for fn in glob.glob("data/**/*.txt"):
         if fn in existing_docs:
             continue
         loader = TextLoader(fn)
         documents = loader.load()
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1000, chunk_overlap=200
+        )
         docs = text_splitter.split_documents(documents)
         logging.info(f"generate embeddings for {fn}")
         await db.aadd_documents(docs)
@@ -154,7 +164,9 @@ async def populate_db(db):
 
 async def load_db():
     # Use Llama model for embedding
-    embeddings = LlamaCppEmbeddings(model_path="llama-2-7b-chat/ggml-model-f16.gguf", n_ctx=2048, verbose=False)
+    embeddings = LlamaCppEmbeddings(
+        model_path="llama-2-7b-chat/ggml-model-f16.gguf", n_ctx=2048, verbose=False
+    )
     db = Chroma(persist_directory="./db", embedding_function=embeddings)
     await populate_db(db)
     return db
@@ -167,10 +179,7 @@ async def query_db(question: str):
 
 
 @app.post("/chat_with_documents/")
-async def chat_with_documents(
-    input: str,
-    username: str = Depends(authenticate_user)
-):
+async def chat_with_documents(input: str, username: str = Depends(authenticate_user)):
     template = """Please give a short answer using the context enclosed in <ctx></ctx>.
     
     <ctx>
@@ -183,23 +192,26 @@ async def chat_with_documents(
 
     docs = await query_db(input)
 
-    prompt = PromptTemplate(template=template, input_variables=["question", "summaries"])
+    prompt = PromptTemplate(
+        template=template, input_variables=["question", "summaries"]
+    )
     callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
     llm = LlamaCpp(
         # model downloaded from meta and
         # converted with https://github.com/ggerganov/llama.cpp/blob/master/convert.py
         model_path="llama-2-7b-chat/ggml-model-f16.gguf",
         temperature=0.75,
-        n_ctx = 1024,
+        n_ctx=1024,
         max_tokens=2000,
         top_p=1,
-        callback_manager=callback_manager, 
-        verbose=True, # Verbose is required to pass to the callback manager
+        callback_manager=callback_manager,
+        verbose=True,  # Verbose is required to pass to the callback manager
     )
     llm_chain = LLMChain(prompt=prompt, llm=llm)
 
     resp = await llm_chain.arun({"question": input, "summaries": docs})
     return resp
-    
+
+
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True, workers=2)
